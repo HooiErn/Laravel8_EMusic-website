659f2aea2b27c757bab837e1266145226dfede9e
HooiErn <d200217c.sc.edu.my>
2022-01-02 18:52:25 +0800

    layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,9 @@
 @extends('layouts.app')
 @section('content')
+<!--Css-->
+<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}"> 
+<div class="content1">
+<h1>Greeting!</h1>
+
+@endsection
+</div>
