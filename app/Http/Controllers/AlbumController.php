35dfe33348c448b7079a732539eb84653790095e
HooiErn <d200217c.sc.edu.my>
2022-01-05 14:35:02 +0800

    addalbum&artist

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use App\Models\Album;
+use Session;
 use Illuminate\Http\Request;
 
 class AlbumController extends Controller
 {
-    //
+    public function add(){
+        $r=request();
+        $addAlbum=Album::create([
+            'name'=>$r->albumName,
+        ]);
+        Session::flash('success',"Album create successfully!");
+        Return view('addAlbum');
+    }
 }
