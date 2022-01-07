0f6e83fb2ffc48b363665da54081ac3bdabdb08d
HooiErn <d200217c.sc.edu.my>
2022-01-07 10:19:13 +0800

    updata artist and ablum

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -11,8 +11,17 @@ class AlbumController extends Controller
 {
     public function add(){
         $r=request();
+        $coverImage=$r->file('coverImage');        
+        $coverImage->move('images',$coverImage->getClientOriginalName());   //images is the location                
+        $imageName=$coverImage->getClientOriginalName(); 
         $addAlbum=Album::create([
             'name'=>$r->albumName,
+            'songID'=>$r->songID,
+            'artistID'=>$r->artistID,
+            'coverImage'=>$coverImage,
+            'description'=>$r->songDescription,
+            'price'=>$r->price,
+            'dateReleased'=>$r->dataReleased,
         ]);
         Session::flash('success',"Album create successfully!");
         Return view('addAlbum');
